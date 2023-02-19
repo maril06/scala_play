221c8adba0744ce51dd83af77cfb1c6c39193db4
maril06 <neckerchief06@gmail.com>
2023-02-19 22:52:25 +0900

    scala worksheet

---
+++ .worksheet/src/test.repl.scala
@@ -0,0 +1,5 @@
+package test
+
+object repl {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(71); 
+  println("Welcome to the Scala worksheet")}
+}
